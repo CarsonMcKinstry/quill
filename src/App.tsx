dc3e3fb72157989f002d9a0a712ad07d68d01a2d
Carson McKinstry <carson.mckinstry@gmail.com>
2026-04-18 10:37:03 +0200

    router now works

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,30 @@
+import {
+  createMemoryHistory,
+  createRouter,
+  RouterProvider,
+} from "@tanstack/react-router";
 import { createRoot } from "react-dom/client";
 
+import { routeTree } from "@/routeTree.gen";
+import { StrictMode } from "react";
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const router = createRouter({
+  defaultPendingMinMs: 0,
+  routeTree,
+  history: createMemoryHistory({
+    initialEntries: ["/"],
+  }),
+});
+
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
--- vite.renderer.config.mts
+++ vite.renderer.config.mts
@@ -1,7 +1,19 @@
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tanstackRouter({
+      target: "react",
+    }),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
